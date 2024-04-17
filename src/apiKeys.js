cfedaa8bd30be3b03919a204da0591c43c84bffc
Alpana Toppo <141553812+AlpanaToppo@users.noreply.github.com>
2024-04-18 00:32:50 +0530

    Basic Project 1 - Weather Forecasting App

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "30d4741c779ba94c470ca1f63045390a",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
